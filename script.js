768be33facace7abad491ccd4e5a47b9008ff79f
MikeWill <m08169071@gmail.com>
2024-11-06 22:01:43 -0500

    Js functionality updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 form = document.getElementById("form");
-emailInput = document.getElementById("email");
+emailInput = document.querySelector('input[type="email"]');
+/*emailInput = document.getElementById("email");*/
 emailErrorMessage = document.querySelector(".email-error-message");
 newsLetterContainer = document.querySelector(".news-letter-container");
 successContainer = document.querySelector(".success-container");
@@ -35,14 +36,14 @@ function validateEmail() {
 }
 
 function error() {
-  emailInput.style.backgroundColor = "#ffe7e6";
-  emailInput.style.border = "1.5px solid #f32720";
+  /*  emailInput.style.backgroundColor = "#ffe7e6";
+  emailInput.style.border = "1.5px solid #f32720";*/
+  emailInput.classList.add("email-error-styling");
   emailErrorMessage.classList.add("email-error-message-active");
 }
 
 function resetErrorStyles() {
   emailInput.value = "";
-  emailInput.style.backgroundColor = "";
-  emailInput.style.border = "";
+  emailInput.classList.remove("email-error-styling");
   emailErrorMessage.classList.remove("email-error-message-active");
 }
